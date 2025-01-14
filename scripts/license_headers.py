fddca6fefc6a136b624850816b79137c68740087
Lukas Voegtle <5764745+voegtlel@users.noreply.github.com>
2025-01-14 15:16:08 +0100

    Fix args to license headers check and extend message

--- scripts/license_headers.py
+++ scripts/license_headers.py
@@ -112,6 +112,7 @@ def main(path: Path, fix: bool) -> None:
     if success:
         print("All files have the correct license header.")
     else:
+        print("Some license headers are missing or incorrect. Run `python scripts/license_headers.py . --fix` to fix.")
         exit(1)
 
 
