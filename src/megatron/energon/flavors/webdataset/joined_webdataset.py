86848ecb374a52e16a5f1843dfa3e94e862fc9ee
Philipp Fischer <pfischer@nvidia.com>
2025-02-27 14:50:20 +0100

    Fix joined webdataset

--- src/megatron/energon/flavors/webdataset/joined_webdataset.py
+++ src/megatron/energon/flavors/webdataset/joined_webdataset.py
@@ -186,7 +186,7 @@ class JoinedWebdatasetFactory(
 
         dataset = WebdatasetSampleLoaderDataset(
             join_readers=itar_readers,
-            sample_slice_offsets=workers_sample_slice_offsets,
+            workers_sample_slice_offsets=workers_sample_slice_offsets,
             worker_config=self.worker_config,
             shuffle_over_epochs=self.shuffle_over_epochs if self.training else None,
             parallel_slice_iters=parallel_shard_iters,
