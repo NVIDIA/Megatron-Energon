bcc5285cf4fcca8a12d52f9a6a7dc168a87b7247
Philipp Fischer <pfischer@nvidia.com>
2025-03-17 12:44:37 +0100

    Fix optional AV imports

--- src/megatron/energon/av/__init__.py
+++ src/megatron/energon/av/__init__.py
@@ -5,12 +5,10 @@ from megatron.energon.av.decode_av_frames import (
     AVData,
     AVDecoder,
 )
-from megatron.energon.av.fastseek import Fastseek
 from megatron.energon.av.video_data import VideoData
 
 __all__ = [
     "AVData",
     "AVDecoder",
-    "Fastseek",
     "VideoData",
 ]
