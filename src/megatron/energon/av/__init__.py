ac0588a4f97648b34a22b8ccbc9b70b26786deab
Philipp Fischer <pfischer@nvidia.com>
2025-03-25 16:00:50 +0100

    Expose utils

--- src/megatron/energon/av/__init__.py
+++ src/megatron/energon/av/__init__.py
@@ -6,9 +6,12 @@ from megatron.energon.av.av_decoder import (
     AVDecoder,
     AVWebdatasetDecoder,
 )
+from megatron.energon.av.utils import get_clips_uniform, get_single_frames_uniform
 
 __all__ = [
     "AVDecoder",
     "AVWebdatasetDecoder",
     "AVData",
+    "get_clips_uniform",
+    "get_single_frames_uniform",
 ]
