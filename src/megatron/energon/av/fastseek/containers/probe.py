d5c75a9831c551dbfa57c8862aa32cbd8872e6cc
Max Ehrlich <max.ehr@gmail.com>
2025-04-08 12:54:36 -0400

    latest fastseek changes
    Signed-off-by: Max Ehrlich <max.ehr@gmail.com>

--- src/megatron/energon/av/fastseek/containers/probe.py
+++ src/megatron/energon/av/fastseek/containers/probe.py
@@ -1,5 +1,3 @@
-# Copyright (c) 2025, NVIDIA CORPORATION.
-# SPDX-License-Identifier: BSD-3-Clause
 import av
 from sortedcontainers import SortedList
 
@@ -9,14 +7,14 @@ from ..keyframeinfo import KeyframeInfo
 def parse_probe(file):
     keyframes = {}
     with av.open(file) as input_container:
-        for stream in range(len(input_container.streams.video)):
+        for stream_idx, stream in enumerate(input_container.streams.video):
             packet_pts = [
                 (index, p.pts)
-                for index, p in enumerate(input_container.demux(video=stream))
+                for index, p in enumerate(input_container.demux(video=stream_idx))
                 if p.is_keyframe
             ]
             packet_pts.sort(key=lambda x: x[1])
 
-            keyframes[stream] = SortedList([KeyframeInfo(*p) for p in packet_pts])
+            keyframes[stream.id] = SortedList([KeyframeInfo(*p) for p in packet_pts])
 
         return keyframes
