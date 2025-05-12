72fbba8b7f767abb20a47aee48ac4dcb8422d7b1
Lukas Voegtle <5764745+voegtlel@users.noreply.github.com>
2025-05-12 08:17:33 -0700

    Implement filetype guessing decoder

--- src/megatron/energon/flavors/webdataset/sample_decoder.py
+++ src/megatron/energon/flavors/webdataset/sample_decoder.py
@@ -1,13 +1,18 @@
 # Copyright (c) 2025, NVIDIA CORPORATION.
 # SPDX-License-Identifier: BSD-3-Clause
 
-from typing import Any, Literal
+from typing import Any, Callable, Literal
 
 import webdataset
 
 from megatron.energon.av import AVWebdatasetDecoder
 from megatron.energon.cache import FileStoreDecoder
 from megatron.energon.flavors.webdataset.config import split_name_re
+
+try:
+    import filetype
+except ImportError:
+    filetype = None
 
 ImageDecoderType = Literal[
     "l8",
@@ -32,6 +37,46 @@ ImageDecoderType = Literal[
 AVDecoderType = Literal["torch", "AVDecoder", "pyav"]
 
 
+class GuessingHandlerWrapper:
+    """A wrapper that guesses the extension of the file using the `filetype` package."""
+
+    def __init__(self, handler: Callable[[str, bytes], Any]):
+        """
+        Wraps a handler to guess the extension of the file using the `filetype` package.
+
+        Args:
+            handler: The handler to wrap.
+        """
+        self.handler = handler
+        if filetype is None:
+            raise ImportError("filetype is not installed. Install it with `pip install filetype`.")
+
+    def __call__(self, key: str, data: bytes) -> Any:
+        """The handler that guesses the extension of the file using the `filetype` package, then calls the delegate handler."""
+        kind = filetype.guess(data)
+        if kind is not None:
+            key = kind.extension
+        return self.handler(key, data)
+
+    @staticmethod
+    def wrap(
+        active: bool, handlers: list[Callable[[str, bytes], Any]]
+    ) -> list[Callable[[str, bytes], Any]]:
+        """
+        Wraps a list of handlers to guess the extension of the file using the `filetype` package.
+
+        Args:
+            active: Whether to wrap the handlers.
+            handlers: The handlers to wrap.
+
+        Returns:
+            The list of wrapped handlers.
+        """
+        if not active:
+            return handlers
+        return [GuessingHandlerWrapper(handler) for handler in handlers]
+
+
 class SampleDecoder(FileStoreDecoder):
     """The default decoder for webdataset samples."""
 
@@ -41,6 +86,7 @@ class SampleDecoder(FileStoreDecoder):
         image_decode: ImageDecoderType = "torchrgb",
         av_decode: AVDecoderType = "AVDecoder",
         video_decode_audio: bool = False,
+        guess_content: bool = False,
     ):
         """
         Args:
@@ -48,27 +94,37 @@ class SampleDecoder(FileStoreDecoder):
             av_decode: If "AVDecoder", returns an AVDecoder instance for flexible decoding. If "torch",
                 returns decoded VideoData.
             video_decode_audio: Whether to decode audio from video files.
+            guess_content: Whether to guess the contents of the file using the `filetype` package.
         """
         self._config = dict(
             image_decode=image_decode,
             av_decode=av_decode,
             video_decode_audio=video_decode_audio,
+            guess_content=guess_content,
         )
         self._decoder = webdataset.autodecode.Decoder(
-            [
-                webdataset.autodecode.imagehandler(image_decode),
-                AVWebdatasetDecoder(
-                    video_decode_audio=video_decode_audio,
-                    av_decode=av_decode,
-                ),
-            ]
+            GuessingHandlerWrapper.wrap(
+                guess_content,
+                [
+                    webdataset.autodecode.imagehandler(image_decode),
+                    AVWebdatasetDecoder(
+                        video_decode_audio=video_decode_audio,
+                        av_decode=av_decode,
+                        guess_content=guess_content,
+                    ),
+                ],
+            ),
         )
 
     def decode(self, fname: str, raw: bytes) -> Any:
         m = split_name_re.match(fname)
-        if not m:
-            raise ValueError(f"Invalid file name: {fname}")
-        cur_base_name, ext = m.groups()
+        if m:
+            cur_base_name, ext = m.groups()
+        else:
+            # If the file name does not match the expected format, use the file name as is.
+            # The handlers may still handle the file correctly (e.g. if guessing by content).
+            cur_base_name = fname
+            ext = "."
 
         return self(
             {
