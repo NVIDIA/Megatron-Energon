d86cf0ee9212de1a1660c889ab7fa98a1840cbce
Lukas Voegtle <5764745+voegtlel@users.noreply.github.com>
2025-07-03 10:39:46 +0200

    License header update

--- src/megatron/energon/flavors/trace.py
+++ src/megatron/energon/flavors/trace.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.
+# SPDX-License-Identifier: BSD-3-Clause
+
 import functools
 from typing import Any, Callable, Dict, Iterator, List, Optional, Sequence, TypeVar, Union
 
